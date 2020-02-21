712beed9a1431a214286a04149a65cbb2a89bb74
Syed Ashraful Karim <akdalim@yahoo.com>
2020-02-21 20:24:40 +0100

    formatted

--- Addition.java
+++ Addition.java
@@ -1,11 +1,12 @@
 import java.util.*;
-class Addition
-{
-	public static void main(String args[])
-	{	int x=10,y=25;
+
+class Addition {
+
+	public static void main(String args[]) {	
+		int x=10,y=25;
 		System.out.println("Addition of two integers :");
 		//int sum = x+y;
 		System.out.println("Summation : "+ (x+y));
-	 int z = 50; 
+		 int z = 50; 
 	}
 }
