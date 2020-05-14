5fc8d657310e87e5021d4f13871b4afc629c8872
Ahnaf <tahmidulhuq4156@gmail.com>
2020-05-14 17:59:09 +0600

    Write and Read from the same text file

--- test/io/FileTest.java
+++ test/io/FileTest.java
@@ -2,6 +2,7 @@ package test.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,12 +13,22 @@ public class FileTest {
 		try {
 		FileWriter fw = new FileWriter("Test.txt");
 		BufferedWriter bw = new BufferedWriter(fw);
-		String text = "Hello World";
+		String text = "Hello World \n";
 		bw.write(text,0,text.length());
+		String text2 = "To infinity and beyond";
+		bw.write(text2,0,text2.length());
 		bw.close();
 		
+		FileReader fr = new FileReader("Test.txt");
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine();
+	    while (line!=null) {
+	        System.out.println(line);
+	        line = br.readLine();
+	    }
+	    fr.close();
+		
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
